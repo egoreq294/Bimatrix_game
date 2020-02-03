bf872394f0e1203a82c48fad8aa4a190c490b844
egoreq <egoreq@bk.ru>
2020-02-03 15:01:30 +0400

    init: new project

---
+++ script.js
@@ -0,0 +1,11 @@
+let l = () => {
+const a = [[2,2],[3,4]];
+const b = [2,3];
+const x = Array.from(math.lusolve(a,b));
+document.querySelector('#result').innerHTML = '( ';
+for(let i = 0; i < a.length-1; i++)
+    {
+	    document.querySelector('#result').innerHTML += (x[i]+', ');
+    }
+document.querySelector('#result').innerHTML +=x[1] + ' )';
+}
